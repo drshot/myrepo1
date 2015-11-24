fe76752bb0e90d11852657ba9db5dc74966d7195
Swapnil Vaidya <vaidyaswapneel@hotmail.com>
2015-11-24 01:54:47 -0800

    new fail2ban

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,13 +8,9 @@
 #
 
 
-execute "get_fail2ban" do
-	command "rpm -Uvh http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm"
+package "epel-release.noarch" do
+	action :install
 end
-
-# package "epel" do
-# 	action :install
-# end
 
 package "fail2ban" do
 	action :install
@@ -24,20 +20,6 @@ service "fail2ban" do
 	action [:start, :enable]
 end
 
-node['fail2ban']['filters'].each do |name, options|
-template "/etc/fail2ban/filter.d/#{name}.conf" do
-source 'filter.conf.erb'
-variables(failregex: [options['failregex']].flatten, ignoreregex: [options['ignoreregex']].flatten)
-notifies :restart, 'service[fail2ban]'
-end
-end
-
-=begin
-execute "copy_config" do
-	command "cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local"
-end
-=end
-
 template "/etc/fail2ban/jail.local" do
 	source "jail.conf.erb"
 	mode "644"
@@ -46,6 +28,3 @@ template "/etc/fail2ban/jail.local" do
 	action :create
 	notifies :restart, "service[fail2ban]"
 end
-
-
-
